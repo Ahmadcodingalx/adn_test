79271af9ef4ca7483e1b3cffa48968f24676fbd9
goliamelerolande-lab <goliamelerolande@gmail.com>
2026-07-20 17:02:08 +0000

    javascript

---
+++ tech.js
@@ -0,0 +1,10 @@
+let y=2;
+const a=6;
+
+function GOLI( ) {
+    const my_name = "GOLI rolande"
+    let myPhoneNumber = "96 49 37 14"
+
+    return my_name +  "  mon numero de tel est  "  + myPhoneNumber 
+    
+}
